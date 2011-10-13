33e8b281a2035fe977aa8fe4a566a590ca724cb0
Luis Vega <luiscvega@gmail.com>
2011-10-13 15:19:24 -0500

    Fixed css issue for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,15 @@ PollApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+
+
+
+
+  #I CHANGED FALSE TO TRUE IN THE config.assets.compile
+
+
+
 
   # Generate digests for assets URLs
   config.assets.digest = true
